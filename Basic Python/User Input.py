2b6f5efee20ddd7d7284af0684f2021444a260d8
Thai Bao <thaibao1991dn@gmail.com>
2025-05-28 22:46:37 +0700

    a

--- Basic Python/User Input.py
+++
@@ -1,9 +0,0 @@
-# -****************************** User Input
-print("-********************** User Input")
-# - User Input
-username = input("Enter username:")
-print("Username is: " + username)
-
-# Python <=2.7
-username = raw_input("Enter username:")
-print("Username is: " + username)
--- Python Project/ContentAggregator/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
- 
-# Register your models here.
-admin.site.register(ProgContent)
-admin.site.register(HiringContent)
-admin.site.register(PyContent)
-admin.site.register(CovidContent)
